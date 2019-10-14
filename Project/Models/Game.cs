181bc8ba43d3626e3ba331f901bce5e7689fbab5
Carson Plant <carsonplant22@gmail.com>
2019-10-14 09:35:40 -0600

    semi colon/line breaks

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -11,11 +11,11 @@ namespace ConsoleAdventure.Project.Models
     public void Setup()
     {
       Room one = new Room("one", "Entrance to the tomb, one door to the East");
-      Room two = new Room("two", "First room inside, one door to the South, one door to the East, the tomb seems naturally well lit but there's a torch, may be useful later.");
-      Room three = new Room("three", "It appears to be a dead end, but there seems to be a fragile looking artifact hanging off the back wall; one door to the West back to room 2");
+      Room two = new Room("two", "First room inside, one door to the South, one door to the East, the tomb seems naturally well lit but there's a torch, /n may be useful later.");
+      Room three = new Room("three", "It appears to be a dead end, but there seems to be a fragile looking artifact hanging off the back wall; one door /n to the West back to room 2");
       Room four = new Room("four", "Another dead end, this room may be filled with snakes.; one door to the West back to room 5");
-      Room five = new Room("five", "Interesting, four doors one to the North, South, East, and West...decisions decisions");
-      Room six = new Room("six", "The first completely dark room, a key is hidden until the torch is used to illuminate, one door to the East back to room 5");
+      Room five = new Room("five", "Interesting, four doors; one to the North, South, East, and West...decisions decisions");
+      Room six = new Room("six", "The first completely dark room, a key is hidden until the torch is used to illuminate, one door to the East /n back to room 5");
       Room seven = new Room("seven", "Locked door...something good must be behind(needs key from room 6), inside is the mummy's sarcoughagus (WIN)");
 
 
